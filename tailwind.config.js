6ac4419729e36a80200daffbbdacbc5eabbf4263
Bogdan-Blaj <blajbogdan94@gmail.com>
2022-09-08 14:45:23 +0300

    initial commit with storybook

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", "./stories/**/*.tsx"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
